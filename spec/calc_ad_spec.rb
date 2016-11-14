4c17b7e0d991a8563ea63c3965a23af00f5b1e16
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-11-14 21:11:15 +0200

    add negative tests

--- spec/calc_ad_spec.rb
+++ spec/calc_ad_spec.rb
@@ -34,7 +34,7 @@ describe CalcAd do
     end
   end
 
-  describe '#multipy' do
+  describe '#multiply' do
     it 'two positive number' do
       expect(CalcAd.multiply(7, 3)).to eq 21.0
     end
@@ -67,6 +67,104 @@ describe CalcAd do
 
     it 'on zero' do
       expect(CalcAd.division(6, 0)).to eq 'Dividing by zero is forbidden'
+    end
+  end
+
+  describe 'negative tests' do
+    context 'raise ArgumentError' do
+      context '#addition' do
+        it 'should raise an ArgumentError error if no parameters passed' do
+          expect { CalcAd.addition }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if 1 parameter passed' do
+          expect { CalcAd.addition(6) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if more than 2 parameters passed' do
+          expect { CalcAd.addition(6, 6, 7) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an TypeError if wrong parameter type passed' do
+          expect { CalcAd.addition('str1') }.to raise_error(ArgumentError)
+        end
+      end
+
+      context '#subsctrction' do
+        it 'should raise an ArgumentError error if no parameters passed' do
+          expect { CalcAd.substraction }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if 1 parameter passed' do
+          expect { CalcAd.substraction(6) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if more than 2 parameters passed' do
+          expect { CalcAd.substraction(6, 5, 2) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an TypeError if wrong parameter type passed' do
+          expect { CalcAd.substraction('str1') }.to raise_error(ArgumentError)
+        end
+      end
+
+      context '#mulptiply' do
+        it 'should raise an ArgumentError error if no parameters passed' do
+          expect { CalcAd.multiply }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if 1 parameter passed' do
+          expect { CalcAd.multiply(6) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if more than 2 parameters passed' do
+          expect { CalcAd.multiply(6, 5, 2) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an TypeError if wrong parameter type passed' do
+          expect { CalcAd.multiply('str1') }.to raise_error(ArgumentError)
+        end
+      end
+
+      context '#division' do
+        it 'should raise an ArgumentError error if no parameters passed' do
+          expect { CalcAd.division }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if 1 parameter passed' do
+          expect { CalcAd.division(6) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an ArgumentError error if more than 2 parameters passed' do
+          expect { CalcAd.division(6, 5, 2) }.to raise_error(ArgumentError)
+        end
+
+        it 'should raise an TypeError if wrong parameter type passed' do
+          expect { CalcAd.division('str1') }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    context 'raise TypeError and NoMethodError' do
+      it 'should raise an TypeError if 1 wrong parameter type passed' do
+        expect { CalcAd.addition(6, 'str') }.to raise_error(TypeError)
+      end
+
+      it 'should raise an TypeError if 1 wrong parameter type passed' do
+        expect { CalcAd.substraction(6, 'str') }.to raise_error(TypeError)
+      end
+
+      it 'should raise an TypeError if 1 wrong parameter type passed' do
+        expect { CalcAd.multiply(6, 'str') }.to raise_error(TypeError)
+      end
+
+      it 'should raise an NoMethodError if 1nd operand wrong parameter type passed' do
+        expect { CalcAd.multiply('str', 6) }.to raise_error(NoMethodError)
+      end
+
+      it 'should raise an NoMethodError if 2nd operand wrong parameter type passed' do
+        expect { CalcAd.division(6, 'str') }.to raise_error(NoMethodError)
+      end
     end
   end
 end
